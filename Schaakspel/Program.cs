40f60c9005b391cfbdb15c1761e6bb751bd7413e
Lucaskuster <lucaskuster34@gmail.com>
2022-09-19 13:55:59 +0200

    Classen aangemaakt #1

--- Schaakspel/Program.cs
+++ Schaakspel/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+String[] stukken = { "b" };
+String [] vakken = {"a"};
+String[,] bord =
+{
+    { },
+    { }
+};
+
